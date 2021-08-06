62002a12105edbec2425155d5e2ba59ea029fca6
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-08-06 22:06:34 +0100

    implemented sorting and field limit feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,15 +8,31 @@ const Tour = require('../models/tourModel.js');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD A QUERY
-    //1) FILTERING
+    //1A) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    //2) ADVANCED FILTERING
+    //1B) ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     //EXECUTE A QUERY
     const tours = await query;
