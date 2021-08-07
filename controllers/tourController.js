2655214b256debb2b0bcf7f612e11e6d81c8673b
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-08-07 11:27:17 +0100

    created aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -104,3 +104,31 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStat = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: e,
+    });
+  }
+};
